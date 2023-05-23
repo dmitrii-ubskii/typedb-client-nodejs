typedb/typedb-driver-rust

new release notes tooling (pre-dependency propagation)

tools/release/create_notes.sh
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Vaticle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+ bazel run @vaticle_dependencies//tool/release/notes:create -- vaticle typedb-client-rust HEAD $(cat VERSION) ./RELEASE_TEMPLATE.md ./LATEST_RELEASE_NOTES.md

dmitrii-ubskii/typedb-client-nodejs

tools/release/create_notes.sh
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Vaticle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+ bazel run @vaticle_dependencies//tool/release/notes:create -- vaticle typedb-client-nodejs HEAD $(cat VERSION) ./RELEASE_TEMPLATE.md ./LATEST_RELEASE_NOTES.md